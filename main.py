c381d5a7b5fec9f545b0476e162cc793e9ffe27c
slena94 <lena.sandro@gmail.com>
2024-04-11 21:30:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+from flask import Flask, request, render_template
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import os
+from werkzeug.utils import secure_filename
 
-# Charger le modèle enregistré
+app = Flask(__name__)
+test
+
+# Chemin pour enregistrer les fichiers temporaires
+UPLOAD_FOLDER = 'temp_images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# S'assurer que le dossier d'upload existe
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Charger le modèle
 model = tf.keras.models.load_model('modele_couverture_livre.h5')
 
-# Charger une image de test (remplacez 'chemin_vers_votre_image.jpg' par le chemin de votre image)
-image_path = './livre/1.jpg'
-img = image.load_img(image_path, target_size=(64, 64))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)  # Convertir en lot unique
-img_array /= 255.0  # Normaliser l'image
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Récupérer l'image du formulaire
+        file = request.files['file']
+        if file:
+            # Sécuriser le nom du fichier et le sauvegarder temporairement
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
 
-# Faire une prédiction
-prediction = model.predict(img_array)
+            # Charger l'image depuis le fichier temporaire
+            img = image.load_img(filepath, target_size=(64, 64))
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array /= 255.0  # Normaliser
 
-# Afficher le résultat
-if prediction[0][0] > 0.5:
-    print("Ce livre est prédit comme un bon livre.")
-else:
-    print("Ce livre est prédit comme un mauvais livre.")
+            # laprédiction
+            prediction = model.predict(img_array)
+
+            # Supprimer le fichier temporaire
+            os.remove(filepath)
+
+            #affichage
+            result = "Ce livre est prédit comme un bon livre." if prediction[0][0] > 0.5 else "Ce livre est prédit comme un mauvais livre."
+
+            # Renvoyer la page result
+            return render_template('result.html', result=result)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
